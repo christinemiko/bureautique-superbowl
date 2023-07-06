e94ffb412338c77dea883a567a5bc34d6bf9033e
Christine Chau <christinemiko@gmail.com>
2023-07-06 23:56:19 +0200

    add files html, css and fonts

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-// main.js
-
-// Modules de controle du cycle de vie de l'application et de création 
-// de fenêtre native de navigateur
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   // Création de la fenêtre de navigateur.
@@ -11,35 +7,29 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      contentSecurityPolicy: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com; script-src 'self' 'unsafe-inline'"
     }
-  })
+  });
 
-  // et chargement de l'index.html de l'application.
-  mainWindow.loadFile('index.html')
+  // et chargement du login.html de l'application.
+  mainWindow.loadFile('login.html');
 
   // Ouvrir les outils de développement.
   // mainWindow.webContents.openDevTools()
-}
+  mainWindow.webContents.openDevTools({ mode: 'detach' });
+};
 
-// Cette méthode sera appelée quand Electron aura fini
-// de s'initialiser et sera prêt à créer des fenêtres de navigation.
-// Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
-    // Sur macOS il est commun de re-créer une fenêtre  lors 
+    // Sur macOS il est commun de re-créer une fenêtre lors 
     // du click sur l'icone du dock et qu'il n'y a pas d'autre fenêtre ouverte.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-// Quitter quand toutes les fenêtres sont fermées, sauf sur macOS. Dans ce cas il est courant
-// que les applications et barre de menu restents actives jusqu'à ce que l'utilisateur quitte 
-// de manière explicite par Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// Dans ce fichier vous pouvez inclure le reste du code spécifique au processus principal. Vous pouvez également le mettre dans des fichiers séparés et les inclure ici.
+  if (process.platform !== 'darwin') app.quit();
+});
