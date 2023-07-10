72ddf7e3fdb86cabb958cb641343bb8fcc64ac6c
Christine Chau <christinemiko@gmail.com>
2023-07-10 20:17:07 +0200

    add JWT decode role_sportscaster

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
       preload: path.join(__dirname, 'preload.js'),
       contentSecurityPolicy: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com; script-src 'self' 'unsafe-inline'"
     }
